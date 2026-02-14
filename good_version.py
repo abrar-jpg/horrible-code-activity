00a31f44f120dc4fa62b2e20f81f5a649cfb3fd4
vvenka13 <vvenka13@charlotte.edu>
2026-02-13 23:04:50 -0500

    chore: added addition functionality

--- good_version.py
+++ good_version.py
@@ -5,3 +5,16 @@
 Simple Calculator
 
 '''
+
+def add(num1:float, num2:float) -> float:
+    '''
+    Add 2 floats
+    
+    :param num1: First number to be added
+    :type num1: float
+    :param num2: Second number to be added
+    :type num2: float
+    :return: Sum of num1 and num2
+    :rtype: float
+    '''
+    return num1 + num2
