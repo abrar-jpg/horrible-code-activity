79e77e0700fb82827aff37594a73a5fbaa62e5b8
vvenka13 <vvenka13@charlotte.edu>
2026-02-13 23:05:05 -0500

    chore: added subtraction functionality

--- good_version.py
+++ good_version.py
@@ -18,3 +18,20 @@ def add(num1:float, num2:float) -> float:
     :rtype: float
     '''
     return num1 + num2
+
+
+def subtract(num1:float, num2:float) -> float:
+    '''
+    Subtract 2 floats
+    
+    :param num1: First number to be subtracted
+    :type num1: float
+    :param num2: Second number to be subtracted
+    :type num2: float
+    :return: Difference between num1 and num2
+    :rtype: float
+    '''
+    
+    return num1 - num2
+
+
