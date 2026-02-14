c91041778ef6264e57e62c1ae199f0c922c8ff99
vvenka13 <vvenka13@charlotte.edu>
2026-02-13 23:05:36 -0500

    chore: added division functionality

--- good_version.py
+++ good_version.py
@@ -50,3 +50,18 @@ def multiply(num1:float, num2:float) -> float:
     return num1 * num2
 
 
+def divide(num1:float, num2:float) -> float:
+    '''
+    Divide 2 floats
+    
+    :param num1: First number to be divided; dividend
+    :type num1: float
+    :param num2: Second number to be divided; divisor
+    :type num2: float
+    :return: Quotient of num1 divided by num2
+    :rtype: float
+    '''
+    
+    return num1 / num2
+
+
